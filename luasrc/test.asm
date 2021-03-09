299aabe490d3d56366ad63ec32b07256be24d4f0
astralorchid <61172763+astralorchid@users.noreply.github.com>
2021-03-09 18:51:28 -0500

    Add files via upload

--- luasrc/test.asm
+++ luasrc/test.asm
@@ -1,12 +1,1 @@
-start:
-    xor ax, ax
-    mov ds, ax
-    mov es, ax
-    xor bp, bp
-    xor sp, sp
-
-    mov bx, testString
-    mov ax, [bx]
-    mov [0xb800], al
-    hlt
-testString db 'Hello World!', 0
+add al, [bx]
